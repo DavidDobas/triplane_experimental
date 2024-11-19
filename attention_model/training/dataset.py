bbb342b4093ef601d7309dc367c1ccde95c4a115
David Dobas <daviddobas@seznam.cz>
2024-11-19 22:08:45 +0100

    Fix 2

--- attention_model/training/dataset.py
+++ attention_model/training/dataset.py
@@ -234,23 +234,56 @@ class ImageFolderDataset(Dataset):
     @staticmethod
     def _file_ext(fname):
         return os.path.splitext(fname)[1].lower()
+
     def _get_zipfile(self):
         assert self._type == 'zip'
         if self._zipfile is None:
             self._zipfile = zipfile.ZipFile(self._path)
         return self._zipfile
+
     def _open_file(self, fname):
         if self._type == 'dir':
             return open(os.path.join(self._path, fname), 'rb')
         if self._type == 'zip':
             return self._get_zipfile().open(fname, 'r')
         return None
+
     def close(self):
         try:
             if self._zipfile is not None:
                 self._zipfile.close()
         finally:
             self._zipfile = None
+
+    def __getstate__(self):
+        return dict(super().__getstate__(), _zipfile=None)
+
+    def _load_raw_image(self, raw_idx):
+        fname = self._image_fnames[raw_idx]
+        with self._open_file(fname) as f:
+            if pyspng is not None and self._file_ext(fname) == '.png':
+                image = pyspng.load(f.read(), format='RGB')
+            else:
+                image = np.array(PIL.Image.open(f))
+        if image.ndim == 2:
+            image = image[:, :, np.newaxis] # HW => HWC, doesn't work for gray cars too, original code
+            #image = np.stack((image,)*3, axis=-1) # HW => HWC, but C is not 1, but 3 - for gray images, my older solution
+        image = image.transpose(2, 0, 1) # HWC => CHW
+        return image
+
+    def _load_raw_labels(self):
+        fname = 'dataset.json'
+        if fname not in self._all_fnames:
+            return None
+        with self._open_file(fname) as f:
+            labels = json.load(f)['labels']
+        if labels is None:
+            return None
+        labels = dict(labels)
+        labels = [labels[fname.replace('\\', '/')] for fname in self._image_fnames]
+        labels = np.array(labels)
+        labels = labels.astype({1: np.int64, 2: np.float32}[labels.ndim])
+        return labels
     
 class PairwiseImageDataset(torch.utils.data.Dataset):
     def __init__(self, dataset, size=None):
